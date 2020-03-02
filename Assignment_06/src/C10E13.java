bac4f7372d30158884e7de777b54b9f9155c8995
Yuqi Wang <wangyq961129@outlook.com>
2020-03-02 16:16:05 -0500

    Assignment_06

--- Assignment_06/src/C10E13.java
+++ Assignment_06/src/C10E13.java
@@ -1,2 +1,31 @@
 public class C10E13 {
+    public class MyRectangle2D{
+        private double x;
+        private double y;
+        private double width;
+        private double height;
+
+        public MyRectangle2D(){
+            this.x = 0;
+            this.y = 0;
+            this.width = 1;
+            this.height = 1;
+        }
+
+        public void setX(double x){this.x = x;}
+
+        public void setY(double y){this.y = y;}
+
+        public void setWidth(double w){this.width = w;}
+
+        public void setHeight(double h){this.height = h;}
+
+        public double getX(){return this.x;}
+
+        public double getY(){return this.y;}
+
+        public double getWidth(){return this.width;}
+
+        public double getHeight(){return this.height;}
+    }
 }
